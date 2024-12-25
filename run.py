c50cd068d73200c738cc1c9f045d67cad50d478b
dachai1985 <dachai1985@sohu.com>
2024-12-25 14:21:29 +0800

    pytest new repository

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     report_path = Conf.get_report_path() + os.sep + "result"
     # 构造 HTML 报告的路径
     html_path = Conf.get_report_path() + os.sep + "html"
-    print("测试报告路径111：", report_path)
+    print("测试报告路径：", report_path)
     print("HTML 报告路径：", html_path)
     # 运行测试用例
     pytest.main(["-s", "--alluredir", report_path, "--clean-alluredir"])
